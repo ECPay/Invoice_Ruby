b32348c1bc0e4f93119158d3e58f0bb57605cc47
adela.chang <adela.chang@ecpay.com.tw>
2020-03-05 10:55:37 +0800

    更新rake版本至12.3.3 #76791

--- ecpay_invoice-2.0.0/lib/ecpay_invoice.rb
+++
@@ -1,7 +0,0 @@
-require "ecpay_invoice/version"
-require "ecpay_invoice/invoice_client"
-require "ecpay_invoice/query_client"
-require "ecpay_invoice/notify_client"
-
-module ECpayInvoice
-end
--- ecpay_invoice-2.0.0/lib/ecpay_invoice/error.rb
+++
@@ -1,8 +0,0 @@
-module ECpayErrorDefinition
-  # Generic ECpay exception class.
-  class ECpayError < StandardError; end
-  class ECpayMissingOption < ECpayError; end
-  class ECpayInvalidMode < ECpayError; end
-  class ECpayInvalidParam < ECpayError; end
-    class ECpayInvoiceRuleViolate < ECpayError; end
-end
